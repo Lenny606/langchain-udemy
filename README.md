7781fcc21f7da7bde0512f7772db5d26dbc8c3fd
Tomas Kravcik <kravcik@procorp.cz>
2025-11-08 20:00:20 +0100

    Add `uv.lock` file for dependency management

---
+++ README.md
@@ -0,0 +1,10 @@
+using uv package manager
+- uv init
+- uv add <name>
+
+
+Ollama cmds
+- ollama list
+- ollama pull gemma3:270m
+- ollama run gemma3:270m #spins instance
+- ollama stop
--- main.py
+++ main.py
@@ -1,5 +1,44 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+
+load_dotenv()
+
+
 def main():
-    print("Hello from langchain-udemy!")
+    information = """"
+    Albert Einstein was born in Berlin, Germany. Famous scients.
+    """
+
+    template = """
+    given info in {information}
+    - return short biography
+    - + return 2 fun facts
+    """
+
+    prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=template
+    )
+
+    # llm = ChatOpenAI(
+    #     temperature=0.5,
+    #     model="gpt-4o-mini"
+    # )
+
+    # ollama
+    llm = ChatOllama(
+        temperature=0.5,
+        model="gemma3:270m"
+    )
+    #     chaining in LCEL
+    chain = prompt_template | llm
+    result = chain.invoke(
+        input={"information": information}
+    )
+
+    print(result.content)
 
 
 if __name__ == "__main__":
