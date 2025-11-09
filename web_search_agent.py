e16ee018f9ef2cf5bf3283ff99978dd5f471adfc
Tomas Kravcik <kravcik@procorp.cz>
2025-11-09 20:24:14 +0100

    update search tool agent

--- web_search_agent.py
+++ web_search_agent.py
@@ -4,11 +4,20 @@ from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 from langchain_tavily import TavilySearch
 from schemas import AgentResponse
+from prompt import REACT_PROMPT_CUSTOM_TEMPLATE
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 
 load_dotenv()
 
 # react_prompt = hub.pull("hwchase17/react")
-
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+custom_prompt = PromptTemplate(template=REACT_PROMPT_CUSTOM_TEMPLATE,
+                               input_variables=["input", "agent_scratchpad",
+                                                "tools", "tool_names"]).partial(
+    format_instructions=output_parser.get_format_instructions()
+)
 # it is Search Tool wrapped as chain
 tools = [TavilySearch()]
 model = "gpt-4o-mini"
@@ -23,13 +32,18 @@ agent = create_agent(
     response_format=AgentResponse,
 )
 
+chain = custom_prompt | agent
+
 
 def run_agent():
     result = agent.invoke({"messages": [
         {"role": "user",
 
          "content": "Search the web for 3 jobs in Lisabon in IT field"}]})
-    print(result)
+
+    # Access structured response from the agent
+    structured = result.get("structured_response", None)
+    print(structured if structured is not None else result)
 
 
 if __name__ == "__main__":
